d53ce29e5d1695122a67fd0740df07c336406ae7
Anna <anna@Annas-MacBook-Air.local>
2026-01-26 12:14:30 +0400

    adds iam

---
+++ iam.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_group" "cmtr_vkkq9lu1_iam_group" {
+  name = "cmtr-vkkq9lu1-iam-group"
+}
+
+resource "aws_iam_policy" "cmtr_vkkq9lu1_iam_policy" {
+  name = "cmtr-vkkq9lu1-iam-policy"
+  policy = templatefile("${path.module}/policy.json", {
+    bucket_name = data.aws_s3_bucket.cmtr_vkkq9lu1_bucket_1769411145.bucket
+  })
+  tags = {
+    Project = "cmtr-vkkq9lu1"
+  }
+}
+
+resource "aws_iam_role" "cmtr_vkkq9lu1_iam_role" {
+  name = "cmtr-vkkq9lu1-iam-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  tags = {
+    Project = "cmtr-vkkq9lu1"
+  }
+}
+
+resource "aws_iam_policy_attachment" "attachment" {
+  name       = "attachment_policy"
+  policy_arn = aws_iam_policy.cmtr_vkkq9lu1_iam_policy.arn
+  roles      = [aws_iam_role.cmtr_vkkq9lu1_iam_role.name]
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.cmtr_vkkq9lu1_iam_role.name
+}
--- storage.tf
+++ storage.tf
@@ -4,3 +4,8 @@ resource "aws_s3_bucket" "cmtr_vkkq9lu1_bucket_1769165647" {
     Project = "cmtr-vkkq9lu1"
   }
 }
+
+
+data "aws_s3_bucket" "cmtr_vkkq9lu1_bucket_1769411145" {
+  bucket = "cmtr-vkkq9lu1-bucket-1769411145"
+}
