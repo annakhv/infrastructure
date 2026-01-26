72ec9b73359cd16a6776182dda274906862620b6
Anna <anna@Annas-MacBook-Air.local>
2026-01-26 12:38:17 +0400

    format code

--- iam.tf
+++ iam.tf
@@ -34,7 +34,7 @@ resource "aws_iam_role" "cmtr_vkkq9lu1_iam_role" {
 
 resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment" {
   policy_arn = aws_iam_policy.cmtr_vkkq9lu1_iam_policy.arn
-  role      = aws_iam_role.cmtr_vkkq9lu1_iam_role.name
+  role       = aws_iam_role.cmtr_vkkq9lu1_iam_role.name
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
