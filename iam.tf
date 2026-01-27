f1b72fb1610fc7cd8f97dfc35040819d00d631a8
Anna <anna@Annas-MacBook-Air.local>
2026-01-27 12:56:53 +0400

    changes:

--- iam.tf
+++ iam.tf
@@ -1,7 +1,9 @@
+/*
+
 resource "aws_iam_group" "cmtr_vkkq9lu1_iam_group" {
   name = "cmtr-vkkq9lu1-iam-group"
 }
-/*
+
 resource "aws_iam_policy" "cmtr_vkkq9lu1_iam_policy" {
   name = "cmtr-vkkq9lu1-iam-policy"
   policy = templatefile("${path.module}/policy.json", {
@@ -13,7 +15,7 @@ resource "aws_iam_policy" "cmtr_vkkq9lu1_iam_policy" {
 }
 
 
- */
+
 resource "aws_iam_role" "cmtr_vkkq9lu1_iam_role" {
   name = "cmtr-vkkq9lu1-iam-role"
   assume_role_policy = jsonencode({
@@ -33,15 +35,20 @@ resource "aws_iam_role" "cmtr_vkkq9lu1_iam_role" {
     Project = "cmtr-vkkq9lu1"
   }
 }
-/*
+
 resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment" {
   policy_arn = aws_iam_policy.cmtr_vkkq9lu1_iam_policy.arn
   role       = aws_iam_role.cmtr_vkkq9lu1_iam_role.name
 }
 
 
- */
+
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "cmtr-vkkq9lu1-iam-instance-profile"
   role = aws_iam_role.cmtr_vkkq9lu1_iam_role.name
 }
+
+
+
+
+ */
