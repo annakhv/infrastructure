0e2da2e30151f8b507a36ff888104f9412f68a99
Anna <anna@Annas-MacBook-Air.local>
2026-01-28 13:57:05 +0400

    corrects security group

--- compute.tf
+++ compute.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "ec2" {
-  instance_type   = "t2.micro"
-  ami             = "ami-0c94855ba95c71c99"
-  security_groups = [data.terraform_remote_state.base_infra.outputs.security_group_id]
-  subnet_id       = data.terraform_remote_state.base_infra.outputs.public_subnet_id
+  instance_type          = "t2.micro"
+  ami                    = "ami-0c94855ba95c71c99"
+  vpc_security_group_ids = [data.terraform_remote_state.base_infra.outputs.security_group_id]
+  subnet_id              = data.terraform_remote_state.base_infra.outputs.public_subnet_id
   tags = {
     terraform = true
     Project   = "cmtr-vkkq9lu1"
