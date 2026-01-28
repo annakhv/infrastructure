d781cddc61ce182d2468c341f1c805475e2b3808
Anna <anna@Annas-MacBook-Air.local>
2026-01-28 14:10:41 +0400

    comit

--- compute.tf
+++ compute.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "ec2_sdfaerg" {
   instance_type          = "t2.micro"
   ami                    = "ami-0c94855ba95c71c99"
   vpc_security_group_ids = [data.terraform_remote_state.base_infra.outputs.security_group_id]
   subnet_id              = data.terraform_remote_state.base_infra.outputs.public_subnet_id
   tags = {
-    terraform = true
+    Terraform = true
     Project   = "cmtr-vkkq9lu1"
   }
 }
